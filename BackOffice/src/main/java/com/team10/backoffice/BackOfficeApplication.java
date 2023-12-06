be6ecb5478a84937ce2f79bfaa0106fa5ac23041
sanghoon1109 <tkdgns7108@email.com>
2023-12-06 10:06:17 +0900

    feature/post 기능 추가

--- BackOffice/src/main/java/com/team10/backoffice/BackOfficeApplication.java
+++ BackOffice/src/main/java/com/team10/backoffice/BackOfficeApplication.java
@@ -2,6 +2,7 @@ package com.team10.backoffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class BackOfficeApplication {
