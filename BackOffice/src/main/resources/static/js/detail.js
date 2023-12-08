24dd3eeee226dea673a98ccc530a519234c741f1
Gwon JunHyeok <jh10253267@gmail.com>
2023-12-09 08:01:01 +0900

    프론트 코드 작성
    전체적인 디자인 소폭 수정  및 게시글 좋아요기능과 댓글 좋아요 기능 구현

--- BackOffice/src/main/resources/static/js/detail.js
+++ BackOffice/src/main/resources/static/js/detail.js
@@ -2,21 +2,53 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-let heartEl = document.getElementById("heart");
-heartEl.addEventListener("click", function () {
-    console.log("clicked!")
-    heartEl.classList.toggle("active");
 
-});
 const urlParams = new URL(location.href).searchParams;
 
 const postId = urlParams.get('id');
 const postGetUrl = `http://localhost:8080/api/posts/${postId}`;
 const commentGetUrl = `http://localhost:8080/api/posts/${postId}/comments`;
+
+let heartEl = document.getElementById("heart");
+heartEl.addEventListener("click", function () {
+    let likeUrl = `http://localhost:8080/api/posts/${postId}/likes`;
+    if(heartEl.classList.contains("active")) {
+        axios.delete(likeUrl);
+    } else {
+        axios.post(likeUrl);
+    }
+    heartEl.classList.toggle("active");
+});
+
+
+let commentContainer = document.querySelector(".comment-each-container");
+commentContainer.addEventListener("click", function(evt) {
+    if(!(evt.target.tagName == "svg" || evt.target.tagName == "path")) return;
+
+    let commentId = evt.target.closest(".comment-box").dataset.commentId;
+    let url = `http://localhost:8080/api/posts/${postId}/comments/${commentId}/likes`;
+
+    if(evt.target.tagName == "svg") {
+        if(evt.target.classList.contains("active")) {
+            axios.delete(url);
+        } else {
+            axios.post(url);
+        }
+        evt.target.classList.toggle("active");
+    } else if(evt.target.tagName == "path") {
+        if(evt.target.closest("svg").classList.contains("active")) {
+            axios.delete(url);
+        } else {
+            axios.post(url);
+        }
+        evt.target.closest("svg").classList.toggle("active");
+    }
+});
+
+
 sendAjax(postGetUrl);
 axios.get(commentGetUrl).then(function (response) {
     let data = response.data.responseBody.data;
-    console.log(data);
     makeCommentTemplate(data);
 });
 
@@ -31,6 +63,7 @@ function sendAjax(url) {
 }
 
 function makeTemplate(data) {
+
     const title = document.querySelector(".board-title");
     const content = document.querySelector(".board-content");
 
@@ -38,18 +71,10 @@ function makeTemplate(data) {
     content.innerText = data.responseBody.data.content;
 }
 
-// async function sendComment() {
-//   const commentPostUrl = `http://localhost:8080/api/posts/${postId}comments`;
-//   const commentContent = document.querySelector(".comment-content").value();
-//   const data = {comment : commentContent};
-//   const response = await axios.post(commentPostUrl, data);
-//   return response.data
-// }
-
 document.querySelector(".btn").addEventListener("click", function () {
     const commentPostUrl = `http://localhost:8080/api/posts/${postId}/comments`;
     const commentGetUrl = `http://localhost:8080/api/posts/${postId}/comments`;
-    let commentContent = document.querySelector(".comment-input .comment-content");
+    let commentContent = document.querySelector(".comment-box .input-content");
     let data = {content: commentContent.value};
     commentContent.value="";
 
